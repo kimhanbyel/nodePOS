28ec8c73ea4439e744f75d0dc0ebb82621cdaa8a
kimhanbyel <cjstk1235426@gmail.com>
2024-10-07 21:06:50 +0900

    1h - auto commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let express = requiue('express');   
-let app = express();
+let express = requiue('express');   // express로 열기
+let app = express();                // express에서
 
 app.listen(3000, function(){
     console.log("Hello, World!");
